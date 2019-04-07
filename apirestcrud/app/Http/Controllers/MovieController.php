5361f1fb4e1bdda601a70bf703e8c619158821dd
Daniel <danielochoa@outlook.com>
2019-04-07 16:01:13 -0500

    Server:laravel - Client:angular

---
+++ apirestcrud/app/Http/Controllers/MovieController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Movie::all();
+    }   
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Save
+        return Movie::create($request->all());
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        $movie = Movie::findOrFail($movie->id);
+        $movie->update($request->all());
+
+        return $movie;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        $movie = Movie::findOrFail($movie->id);
+        $movie->delete();
+
+        return 204;
+    }
+}
